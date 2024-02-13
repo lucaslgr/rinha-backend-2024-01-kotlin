85990979125fc940ffef355533b60409763fc342
Lucas Guimarães <lucas.guimaraes@ifood.com.br>
2024-02-13 17:05:30 -0300

    Implements the script.sql file with DDL and DML queries

---
+++ script.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS clients (
+  id SERIAL PRIMARY KEY,
+  account_limit INT NOT NULL,
+  balance INT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS transactions (
+  id SERIAL PRIMARY KEY,
+  amount INT NOT NULL,
+  type CHAR(1) NOT NULL,
+  description VARCHAR(10) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+  client_id INT NOT NULL REFERENCES clients(id)
+);
+
+CREATE INDEX transactions_client_id_index ON transactions(client_id);
+CREATE INDEX transactions_created_at_index ON transactions(created_at DESC);
+
+DO $$
+  BEGIN
+    INSERT INTO clients (account_limit)
+    VALUES
+      (100000),
+      (80000),
+      (1000000),
+      (10000000),
+      (500000);
+  END;
+$$;
