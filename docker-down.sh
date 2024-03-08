7862a39f8476d3bf345995b62819733ad25b2c2b
Lucas Guimarães <lucas.guimaraes@ifood.com.br>
2024-03-08 00:55:53 -0300

    Implements settings in postgresql.conf using PGTune

--- docker-down.sh
+++ docker-down.sh
@@ -1,3 +1,3 @@
 docker-compose down
 docker rmi -f rinha-back-end-2024-01-api01 rinha-back-end-2024-01-api02
-docker volume rm rinha-back-end-2024-01_db_data
+docker volume rm rinha-back-end-2024-01_pg_db_volume
